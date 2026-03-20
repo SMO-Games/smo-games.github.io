caf0221c1f0c566cfd537ced3f905f6c3afdfe34
SMO-Games <268230670+SMO-Games@users.noreply.github.com>
2026-03-20 19:10:15 +0000

    (try to) fix background flickering

--- index.js
+++ index.js
@@ -269,6 +269,20 @@ function addRow(rowNum){
 }
 
 
+let folder = "";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
+
+
 // prevents guessing and shows dialogue
 function endGame(){
     // prevent further guesses
